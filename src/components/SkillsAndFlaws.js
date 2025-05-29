e686ada71b71e4ccedaa5643d0bcf1a42db7012d
James <thejnjcorp@gmail.com>
2025-05-28 21:56:38 -0400

    Add the ability to remove skills/flaws

--- src/components/SkillsAndFlaws.js
+++ src/components/SkillsAndFlaws.js
@@ -1,32 +1,17 @@
 import Collapsible from 'react-collapsible';
 import starIcon from '../icons/star.svg';
-import { useState, useEffect, useRef, useReducer } from 'react';
-import { auth, db } from '../utils/firebase';
+import { useState, useReducer } from 'react';
+import { db } from '../utils/firebase';
 import '../styles/SkillsAndFlaws.scss';
 import { formReducer } from './NewCharacterPage';
-import { arrayUnion, doc, updateDoc } from 'firebase/firestore';
+import { arrayRemove, arrayUnion, doc, updateDoc } from 'firebase/firestore';
+import trashCanIcon from '../icons/trash_can.svg';
 
 export function SkillsAndFlaws({characterPage, userId}) {
     const [addSkillFLawVisible, setAddSkillFlawVisible] = useState(false);
     const [removeSkillFlawVisible, setRemoveSkillFlawVisible] = useState(false);
+    const [confirmRemoveSkillFlaw, setConfirmRemoveSkillFlaw] = useState(false);
     const [formData, setFormData] = useReducer(formReducer, {});
-    const divRef = useRef(null);
-
-    useEffect(() => {
-        document.addEventListener('mousedown', clickOutsidePopupScreen);
-
-        return () => {
-            document.removeEventListener('mousedown', clickOutsidePopupScreen);
-        }
-        // eslint-disable-next-line
-    },[location]);
-
-    const clickOutsidePopupScreen = (event) => {
-        if (divRef.current && !divRef.current.contains(event.target)) {
-            setAddSkillFlawVisible(false);
-            setRemoveSkillFlawVisible(false);
-        }
-    }
 
     const hasWritePermissions = characterPage?.canWrite?.includes(userId);
 
@@ -51,7 +36,7 @@ export function SkillsAndFlaws({characterPage, userId}) {
     return <>
         {hasWritePermissions && <><div className='SkillsAndFlaws-button-container SkillFlawButtonContainerOverride'>
            <button className='SkillsAndFlaws-button SkillFlawButtonOverride' onClick={() => setAddSkillFlawVisible(true)}>Add</button>
-           <button className='SkillsAndFlaws-button SkillFlawButtonOverride' onClick={() => setRemoveSkillFlawVisible(true)}>Remove</button>
+           <button className='SkillsAndFlaws-button SkillFlawButtonOverride' onClick={() => setRemoveSkillFlawVisible(!removeSkillFlawVisible)}>{removeSkillFlawVisible ? "Cancel" : "Remove"}</button>
         </div>
         <br/>
         {addSkillFLawVisible && <div className='SkillsAndFlaws-add-menu SkillFlawButtonAddMenuOverride'>
@@ -103,6 +88,21 @@ export function SkillsAndFlaws({characterPage, userId}) {
             }}>Add</button>
             <button className='SkillsAndFlaws-button SkillFlawButtonOverride' onClick={() => setAddSkillFlawVisible(false)}>Cancel</button>
         </div>}
+        {confirmRemoveSkillFlaw && <div className='SkillsAndFlaws-remove-skill-flaw-confirm SkillFlawRemoveSkillFlawConfirmOverride'>
+            Are you sure you want to delete "{confirmRemoveSkillFlaw.name}"?<br/>
+            <button className='SkillsAndFlaws-button SkillFlawButtonOverride' onClick={async () => {
+                try {
+                    await updateDoc(doc(db, "characters", characterPage.character_id), {
+                        skills_and_flaws: arrayRemove(confirmRemoveSkillFlaw)
+                    });
+                    setRemoveSkillFlawVisible(false);
+                    setConfirmRemoveSkillFlaw(false);
+                } catch (e) {
+                    alert(e);
+                }
+            }}>Yes</button>
+            <button className='SkillsAndFlaws-button SkillFlawButtonOverride' onClick={() => setConfirmRemoveSkillFlaw(false)}>No</button>
+        </div>}
         </>}
         {characterPage.skills_and_flaws.map((skill_or_flaw, index, isOpen=false) => 
             <Collapsible 
@@ -113,6 +113,13 @@ export function SkillsAndFlaws({characterPage, userId}) {
                         <img key={index} src={starIcon} alt='star' className='SkillsAndFlaws-star' width={30}/>
                     ))}
                 </span>
+                {removeSkillFlawVisible && <button className='SkillsAndFlaws-trash-button SkillFlawTrashButtonOverride'
+                    onClick={(e) => {
+                        e.stopPropagation();
+                        setConfirmRemoveSkillFlaw(skill_or_flaw);
+                    }}>
+                    <img key={index} src={trashCanIcon} alt='trash' className='SkillsAndFlaws-trash-can' width={30}/>
+                </button>}
                 </>}
                 className={skill_or_flaw.isSkill ? "SkillsAndFlaws SkillsAndFlaws-skill SkillsOverride" : "SkillsAndFlaws SkillsAndFlaws-flaw FlawsOverride"}
                 openedClassName={skill_or_flaw.isSkill ? "SkillsAndFlaws SkillsAndFlaws-skill SkillsOverride" : "SkillsAndFlaws SkillsAndFlaws-flaw FlawsOverride"}
