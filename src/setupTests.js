4216d3e83e25a6c9a942385158822bf63b9aee26
James <thejnjcorp@gmail.com>
2026-08-22 00:26:54 -0400

    address security vulnerabilities

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,7 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+// jsdom's test environment doesn't expose TextEncoder/TextDecoder, which react-router needs.
+import { TextEncoder, TextDecoder } from 'util';
+Object.assign(global, { TextEncoder, TextDecoder });
